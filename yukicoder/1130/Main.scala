07a2f18e6ea86ed5dd993c9f8a22d255b6d657dd
yumechi(M.H) <yumechi0525@gmail.com>
2021-02-24 00:07:35 +0900

    Create Main.scala

---
+++ yukicoder/1130/Main.scala
@@ -0,0 +1,21 @@
+import java.util.Scanner
+
+object Main extends App {
+  def calc(ar: Array[Int], w: Int): Array[String] = {
+    if(ar.length > w) {
+      val f = ar.splitAt(w)
+      Array(f._1.mkString(" ")).appendedAll(calc(f._2, w))
+    } else {
+      Array(ar.mkString(" "))
+    }
+  }
+
+  val sc = new Scanner(System.in)
+
+  val h = sc.nextInt
+  val w = sc.nextInt
+  val arr = Array.fill(h)(Array.fill(w)(sc.nextInt))
+  val ar = arr.flatten.sorted
+
+  println(calc(ar, w).map(_.mkString("")).mkString("\n"))
+}
