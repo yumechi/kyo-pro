89d97172563ac0b71eac22059fff567b6877be39
yumechi(M.H) <yumechi0525@gmail.com>
2021-02-24 00:19:03 +0900

    Update Main.scala

--- yukicoder/1130/Main.scala
+++ yukicoder/1130/Main.scala
@@ -1,22 +1,14 @@
 import java.util.Scanner
 
 object Main extends App {
-  // なんかもう少しうまく書ける気がする（ Array の操作回りもう少しうまくできる気がする）
-  def calc(ar: Array[Int], w: Int): Array[String] = {
-    if(ar.length > w) {
-      val f = ar.splitAt(w)
-      Array(f._1.mkString(" ")).appendedAll(calc(f._2, w))
-    } else {
-      Array(ar.mkString(" "))
-    }
-  }
-
   val sc = new Scanner(System.in)
 
   val h = sc.nextInt
   val w = sc.nextInt
   val arr = Array.fill(h)(Array.fill(w)(sc.nextInt))
-  val ar = arr.flatten.sorted
+  // groupby で分割できる
+  // https://docs.scala-lang.org/ja/overviews/collections/trait-iterable.html
+  val ar = arr.flatten.sorted grouped w
 
-  println(calc(ar, w).map(_.mkString("")).mkString("\n"))
+  println(ar.map(_.mkString(" ")).mkString("\n"))
 }
