5228d3fbfd115409a4c4b506fefb3a155311e90f
yumechi(M.H) <yumechi0525@gmail.com>
2021-03-01 01:55:08 +0900

    Update Main.scala

--- yukicoder/1047/Main.scala
+++ yukicoder/1047/Main.scala
@@ -3,12 +3,12 @@ import java.util.Scanner
 object Main extends App {
   val sc = new Scanner(System.in)
 
-  val a = sc.nextInt
-  val b = sc.nextInt
+  val a = sc.nextLong
+  val b = sc.nextLong
 
 
-  def solve(a: Int, b: Int): Int = {
-    var n = 0
+  def solve(a: Long, b: Long): Int = {
+    var n = 0L
     for(i <- 1 to 3) {
       n = a * n + b
       if(n == 0) return i
