7176f3cfca5d6f404f8db0b2894525016305b42f
yumechi(M.H) <yumechi0525@gmail.com>
2021-03-01 23:36:34 +0900

    Update Main.scala

--- yukicoder/401/Main.scala
+++ yukicoder/401/Main.scala
@@ -5,40 +5,17 @@ object Main extends App {
 
   val n = sc.nextInt
   var an = Array.fill(n, n)(0)
-  var x = 0
-  var y = 0
-  var mode = 1
+  var x: Int = 0
+  var y: Int = 0
+  val mode = Array(Array(1, 0), Array(0, 1), Array(-1, 0), Array(0, -1))
+  var m: Int = 0
   for (i <- 1 to n * n) {
     an(y)(x) = i
-    if (mode == 1) {
-      if (x == n - 1 || (x < n - 1 && an(y)(x + 1) != 0)) {
-        mode = 2
-        y += 1
-      } else {
-        x += 1
-      }
-    } else if (mode == 2) {
-      if (y == n - 1 || (y < n - 1 && an(y + 1)(x) != 0)) {
-        mode = 3
-        x -= 1
-      } else {
-        y += 1
-      }
-    } else if (mode == 3) {
-      if (x == 0 || (x > 0 && an(y)(x - 1) != 0)) {
-        mode = 4
-        y -= 1
-      } else {
-        x -= 1
-      }
-    } else if (mode == 4) {
-      if (y == 0 || (y > 0 && an(y - 1)(x) != 0)) {
-        mode = 1
-        x += 1
-      } else {
-        y -= 1
-      }
-    }
+    val tx = x + mode(m)(0)
+    val ty = y + mode(m)(1)
+    if(tx > n - 1 || ty > n - 1 || tx < 0 || ty < 0 || an(ty)(tx) != 0) m = (m + 1) % 4
+    x += mode(m)(0)
+    y += mode(m)(1)
   }
   println(an.map(a => a.map(b => "%03d".format(b)).mkString(" ")).mkString("\n"))
 }
