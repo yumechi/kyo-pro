a418d9f504abfc4901928938fc7db0db2c9862c9
yumechi(M.H) <yumechi0525@gmail.com>
2021-03-01 23:53:34 +0900

    Update Main.scala

--- yukicoder/1089/Main.scala
+++ yukicoder/1089/Main.scala
@@ -4,14 +4,5 @@ object Main extends App {
   val sc = new Scanner(System.in)
 
   val n = sc.nextInt
-  var ans = 0
-  for(i <- 0 to n) {
-    for(j <- 0 to (n - i)) {
-      val k = n - (i + j)
-      if(i + j + k == n) {
-        ans += 1
-      }
-    }
-  }
-  println(ans)
+  println((0 to n).toList.map(i => (0 to (n - i)).toList.count(j => n - i - j >= 0)).sum)
 }
