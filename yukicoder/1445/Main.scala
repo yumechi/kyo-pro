349bbc5cfc1941a4a8bffd3f996e81f6dc531e3a
yumechi(M.H) <yumechi0525@gmail.com>
2021-04-08 01:20:20 +0900

    Create Main.scala

---
+++ yukicoder/1445/Main.scala
@@ -0,0 +1,9 @@
+import java.util.Scanner
+
+object Main extends App {
+  val sc = new Scanner(System.in)
+
+  val n = sc.nextInt
+
+  for(_ <- 0 until n) println("Hello! You're new here, right? It's nice to meet you.")
+}
